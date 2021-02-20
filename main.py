dcd48cdb298f34b79b2a97c01383da726f576a72
Siddhartha Chatterjee <62523596+siddharthachatterjee@users.noreply.github.com>
2021-02-19 16:40:39 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+def move(n, source, target, aux):
+  if n > 0:
+    move(n - 1, source, aux, target)
+    print("Move disk {} from {} to {}".format(n, source, target))
+    move(n - 1, aux, target, src)
